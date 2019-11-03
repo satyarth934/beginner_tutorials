2d4db686e3ec1cc95d2a2ca271d88307233f2ee8
Satyarth Praveen <satyarth934@gmail.com>
2019-11-03 14:35:06 +0000

    [SERVICE DEMO] Code to demonstrate the use of ROS Service. This service, whenever called, changes the string message published by the talker node.

--- src/talker.cpp
+++ src/talker.cpp
@@ -8,13 +8,19 @@
  * @version 1.0
  * @brief   Publisher Node
  * @section DESCRIPTION
- * A C++ implementation to demonstrate simple sending of messages over the ROS system.
+ * A C++ implementation to demonstrate the usage of services.
  */
 
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <beginner_tutorials/ChangeString.h>
 
-#include <sstream>
+std::string str_msg("Go Terps :D");
+
+bool changeString(beginner_tutorials::ChangeString::Request  &req,
+                  beginner_tutorials::ChangeString::Response &res) {
+  str_msg = req.in_msg;
+}
 
 /**
  * @brief An implementation to demonstrate simple sending of messages over the ROS system.
@@ -23,40 +29,39 @@
  * @return 0 Return 0 for successful execution.
  */
 int main(int argc, char **argv) {
+
   /// Initializing a ros node.
   ros::init(argc, argv, "talker");
 
   /// Creating an instance of the NodeHandle.
   ros::NodeHandle n;
-
+ 
   /// Creating a publisher with the topic name "chatter" and buffer size 1000.
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
+  /// Creating a service server with the service name "change_string"
+  ros::ServiceServer service = n.advertiseService("change_string", changeString);
+
+  /// Initializing the msg to be published.
+  std_msgs::String msg;
+
+  ROS_DEBUG_STREAM("Declaration of publisher and service successful.");
+
   /// Defining the loop_rate to be 10.
-  ros::Rate loop_rate(10);
-
-  /// To count the number of messages sent.
-  int count = 0;
+  int looprate = 10;
+  ros::Rate loop_rate(looprate);
   while (ros::ok()) {
-    /// This is the message object that is published on the "chatter" topic.
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "Go Terps!!! " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
+    ROS_DEBUG_STREAM("Publishing Message: " << str_msg);
+    /// setting the value of the message to be published.
+    msg.data = str_msg;
 
     /// The publish() function publishes the messages over the "chatter" topic.
     chatter_pub.publish(msg);
-
     ros::spinOnce();
 
     /// Sleeps at regular intervals to get desired effective frequency (10hz).
     loop_rate.sleep();
-    ++count;
   }
 
   return 0;
-}
-
+} 
